4f7432c6355a4d0dfb1eef95388594193f8543b0
Akram Azkia <molenakram8@gmail.com>
2024-08-17 11:23:29 +0700

    installasi yargs dan beberapa fungsi di npm

--- app.js
+++ app.js
@@ -3,64 +3,59 @@ const fs = require('fs');
 
 const contacts = require('./contacts.js')
 
+const yargs= require('yargs')
 
 
-//try {
-//    fs.writeFileSync('data/text.txt', 'menulis file secara syncronous');
-//} catch (e) {
-//    console.log (e);
+
+
+yargs.command({
+    command : 'tambah',
+    describe : 'menambahkan kontak baru',
+    builder : {
+        nama : {
+            describe : 'Nama Lengkap',
+            demandOption : true,
+            type : 'string',
+
+        },
+        email : {
+            describe : 'email',
+            demmandOption : false,
+            type : 'string'
+        },
+        noHP : {
+            describe : 'Nomor Handphonbe',
+            demmandOption : true,
+            type : 'string'
+        }
+
+    },
+
+    handler(argv) {
+       contacts.simpanKontak(argv.nama, argv.email, argv.noHP)
+    }
+
+})
+
+
+
+yargs.parse()
+
+
+
+
+
+
+
+
+
+
+//const main = async() => {
+//    const nama = await contacts.tulisPertanyaan('masukkan nama anda : ')
+//    const noHP = await contacts.tulisPertanyaan('masukkan noHP anda : ')
+//    const email = await contacts.tulisPertanyaan('masukkan email anda : ')
+
+//    contacts.simpanKontak(nama,noHP,email)
 //}
 
-
-//menuliskan string ke file secara (asyncronous)
-//const akram = ("akram ganteng")
-//fs.writeFile('data/text.txt', 'menuliskan file secara asinkronoussss', (e) => {
-//    console.log(e,akram)
-//});
-
-//membaca isi file secara sinkronous
-
-//const data = fs.readFileSync('data/text.txt','utf-8');
-//console.log(data);
-
-//membaca isi file secara asinkronous
-
-//fs.writeFile('data/text.txt', 'akram ganteng bingitsss', (e) => {
-//    console.log(e)
-//})
-
-//const data = fs.readFile('data/text.txt','utf-8',(e, data) => {
-//    if (e) 
-//        return e
-//    else console.log(data)
-    
-//})
-
-
-//const pertanyaan2 = () => {
-//    return new Promise((resolve, reject) => {
-//        rl.question('masukkan No Hp Anda : ', (noHP) => {
-//            resolve(noHP)
-//        })
-//    })
-//}
-//const pertanyaan3 = () => {
-//    return new Promise((resolve, reject) => {
-//        rl.question('masukkan email anda : ', (email) => {
-//            resolve(email)
-//        })
-//    })
-//}
-
-const main = async() => {
-    const nama = await contacts.tulisPertanyaan('masukkan nama anda : ')
-    const noHP = await contacts.tulisPertanyaan('masukkan noHP anda : ')
-    const email = await contacts.tulisPertanyaan('masukkan email anda : ')
-
-    contacts.simpanKontak(nama,email,noHP)
-}
-
-main()
-
-
-
+//main()
--- contacts.js
+++ contacts.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
+const chalk = require('chalk')
+const validator = require('validator')
 
-const readline= require('readline');                                        //membuat variabel terlebih dahulu sebelum menggunakan module 
-const rl = readline.createInterface({                                       // membuat interface untuk form pertanyaan di terminal
-    input: process.stdin,
-    output: process.stdout
-});
+//const readline= require('readline');                                        //membuat variabel terlebih dahulu sebelum menggunakan module 
+//const rl = readline.createInterface({                                       // membuat interface untuk form pertanyaan di terminal
+//    input: process.stdin,
+//    output: process.stdout
+//});
 
 //membuat folder data
 const dirPath = './data'
@@ -19,22 +21,48 @@ if(!fs.existsSync(dataPath)) {
 
 
 //membuat pertanyaan di dalam prompt
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (jawaban) => {
-            resolve(jawaban)
-        })
-    })
-}
+//const tulisPertanyaan = (pertanyaan) => {
+//    return new Promise((resolve, reject) => {
+//        rl.question(pertanyaan, (jawaban) => {
+//            resolve(jawaban)
+//        })
+//    })
+//}
 // membuat fungsi simpan kontak
-const simpanKontak = (nama, noHP, email) => {
-    const contact = {nama, noHP, email}                                // membuat objek untuk menampung pertanyaan yang telah dibuat dengan membuat variable
+const simpanKontak = (nama, email, noHP) => {
+    const contact = {nama, email, noHP}                                // membuat objek untuk menampung pertanyaan yang telah dibuat dengan membuat variable
     const file = fs.readFileSync ('data/contacts.json', 'utf-8')     // membuat variabel file untuk menginisialisasi dan melihat data-contact yang telah dibuat sebelumnya 
     const contacts = JSON.parse (file)                               // membuat variable contacts kemudian di ubah menjadi tipe file json dengan variabel file
+    
+
+    //cek duplikat pada file json jika ada maka tidak dijalankan
+    const duplikat = contacts.find((contact) => contact.nama === nama )
+
+    if (duplikat) {
+        const peringatan = (chalk.red.inverse.bold("nama sudah digunakan , gunakan nama lain ! "))
+        console.log (peringatan)
+        return false
+    }
+    // fungsi cek email jika tidak ada maka tidak apa apa jika ada namun salah mengisi form email seperti seharusnya maka muncul tampilan
+    if (email) {
+        if (!validator.isEmail(email)){
+            const peringatanEmail= (chalk.red.inverse.bold("EMAIL TIDAK VALID"))
+            console.log(peringatanEmail)
+            return false
+        }
+    }
+    //cek nomor handphone apakah valid
+    if (!validator.isMobilePhone(noHP, 'id-ID')) {
+        const peringatanNohp= (chalk.red.inverse.bold("NOMOR HANDPHONE TIDAK TERDAFTAR"))
+        console.log(peringatanNohp)
+        return false
+    }
+   
     contacts.push(contact)                                           // menyatukan variabel contacts dengan variabel contact
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts)) // menulis apa yang ada di variabel contacts ke dalam file contact.json dan mengubahnya terlebih dahulu dari json menjadi string
-    console.log('terimakasih sudah mengisi')
-    rl.close()
+    const sukses = (chalk.green.inverse.bold('terimakasih sudah mengisi'))
+    console.log(sukses)
+    
 } 
 
-module.exports = {tulisPertanyaan,simpanKontak}
+module.exports = {simpanKontak}
